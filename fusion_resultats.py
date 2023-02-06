69d29f778478c16c2d1965ddd2502e3dc9e005d8
juugeebee <july2023@yahoo.fr>
2023-02-06 12:44:41 +0100

    pb nom de genes

--- fusion_resultats.py
+++ fusion_resultats.py
@@ -45,55 +45,55 @@ for i in range(len(tissus)):
         df = pandas.read_csv(fichiers[i], header=[0], sep='\t')
         del df['length']
         del df['%gc']
-        del df['mean_coverage'] 
+        del df['read_count'] 
         del df['normalized_coverage'] 
         del df['min_normalized_coverage'] 
         del df['max_normalized_coverage']
         del df['min_coverage']
         del df['max_coverage'] 
         del df['pct_0x']
-        df.rename(columns = {'read_count': 'read_count_' + ech}, inplace = True)
+        df.rename(columns = {'mean_coverage': 'mean_coverage_' + ech}, inplace = True)
         
         if not pax :
             pax.append(df)
         else :
-            pax.append(df['read_count_' + ech])
+            pax.append(df['mean_coverage_' + ech])
     
     if tissus[i] == 'fibroblaste':
         df = pandas.read_csv(fichiers[i],header = [0], sep='\t')
         del df['length']
         del df['%gc']
-        del df['mean_coverage'] 
+        del df['read_count'] 
         del df['normalized_coverage'] 
         del df['min_normalized_coverage'] 
         del df['max_normalized_coverage']
         del df['min_coverage']
         del df['max_coverage'] 
         del df['pct_0x']
-        df.rename(columns = {'read_count': 'read_count_' + ech}, inplace = True)
+        df.rename(columns = {'mean_coverage': 'mean_coverage_' + ech}, inplace = True)
 
         if not fibro :
             fibro.append(df)
         else : 
-            fibro.append(df['read_count_' + ech])
+            fibro.append(df['mean_coverage_' + ech])
 
     if tissus[i] == 'lymphocyte':
         df = pandas.read_csv(fichiers[i],header = [0], sep='\t')
         del df['length']
         del df['%gc']
-        del df['mean_coverage'] 
+        del df['read_count'] 
         del df['normalized_coverage'] 
         del df['min_normalized_coverage'] 
         del df['max_normalized_coverage']
         del df['min_coverage']
         del df['max_coverage'] 
         del df['pct_0x']
-        df.rename(columns = {'read_count': 'read_count_' + ech}, inplace = True)
+        df.rename(columns = {'mean_coverage': 'mean_coverage_' + ech}, inplace = True)
 
         if not lympho :
             lympho.append(df)
         else :
-            lympho.append(df['read_count_' + ech])
+            lympho.append(df['mean_coverage_' + ech])
 
 
 df_pax = pandas.concat(pax, axis=1)
@@ -136,28 +136,41 @@ del df_spatax[1]
 del df_spatax[2]
 
 
-df_panel = pandas.concat([df_dips, df_ee, df_spatax])
+df_cibles = pandas.concat([df_dips, df_ee, df_spatax])
 
 
-df_pax_f = df_pax.merge(df_panel, left_on='name', right_on=3)
+df_rnaseq = pandas.read_csv('/media/jbogoin/Data1/References/cibles_panels_NG/RNAseq_UFNeuro_v1_Regions.bed',header = None, sep='\t')
+del df_rnaseq[0]
+del df_rnaseq[1]
+del df_rnaseq[2]
+
+
+df_panel = df_rnaseq.merge(df_cibles, left_on=3, right_on=3, how='left')
+df_panel.to_csv('cibles.csv', sep='\t', index=False)
+
+
+
+df_pax_f = df_pax.merge(df_panel, left_on='name', right_on=3, how='outer')
 del df_pax_f['Panel_x']
 del df_pax_f[3]
 df_pax_f.rename(columns = {'Panel_y': 'Panel'}, inplace = True)
 df_pax_f.sort_values(by=['name', 'start'], inplace=True)
+df_pax_f.drop_duplicates(keep='first', inplace=True, ignore_index=True)
 
 
-df_fibro_f = df_fibro.merge(df_panel, left_on='name', right_on=3)
+df_fibro_f = df_fibro.merge(df_panel, left_on='name', right_on=3, how='outer')
 del df_fibro_f['Panel_x']
 del df_fibro_f[3]
 df_fibro_f.rename(columns = {'Panel_y': 'Panel'}, inplace = True)
 df_fibro_f.sort_values(by=['name', 'start'], inplace=True)
+df_fibro_f.drop_duplicates(keep='first', inplace=True, ignore_index=True)
 
-
-df_lympho_f = df_lympho.merge(df_panel, left_on='name', right_on=3)
+df_lympho_f = df_lympho.merge(df_panel, left_on='name', right_on=3, how='outer')
 del df_lympho_f['Panel_x']
 del df_lympho_f[3]
 df_lympho_f.rename(columns = {'Panel_y': 'Panel'}, inplace = True)
 df_lympho_f.sort_values(by=['name', 'start'], inplace=True)
+df_lympho_f.drop_duplicates(keep='first', inplace=True, ignore_index=True)
 
 
 ## Fichiers finaux
